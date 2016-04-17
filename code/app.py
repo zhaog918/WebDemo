e0a6ec626e448540aabbf0e544c4f7b51020b84c
Gang Zhao <zhaog918@gmail.com>
2016-04-17 18:36:56 +0800

    Modify app.py,add IP Address show

--- code/app.py
+++ code/app.py
@@ -1,13 +1,16 @@
 from flask import Flask
 from redis import Redis
 import os
+import socket
+localIP=socket.gethostbyname(socket.gethostname())
 app = Flask(__name__)
 redis = Redis(host='redis', port=6379)
 
 @app.route('/')
 def hello():
     redis.incr('hits')
-    return 'Hello ! I have been seen %s times.' % redis.get('hits')
+    showword='Hello ! I have been seen %s times.' % redis.get('hits') + 'From IP:' + socket.gethostbyname(socket.gethostname())
+    return '%s' % showword 
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
